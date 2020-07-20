fadd542cddf873a545c4c7255ac9223b9b592828
ShahryarAhmed <ahmedshahryar333@gmail.com>
2020-07-20 12:50:40 +0300

    Create JavaApplication1.java

---
+++ JavaApplication1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+import javax.swing.*; 
+import java.awt.*; 
+import java.awt.event.*;
+import javax.swing.table.*;
+/**
+ *
+ * @author shahryarahmed
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+
+        //opening up the frame
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        
+
+         
+    }
+    
+}
